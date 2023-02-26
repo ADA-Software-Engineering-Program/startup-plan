a82d2fd131fdb004efa1a747f1a77e405ceca4ed
daluclemas <chidaluanukam@gmail.com>
2023-02-26 20:40:35 +0100

    bug fix

--- src/components/Testimony/Testi.jsx
+++ src/components/Testimony/Testi.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./Testi.css";
-import ming from "../../assets/ming.png";
+
 import pic1 from "../../assets/pic1.png";
 import pic2 from "../../assets/pic2.png";
 import pic3 from "../../assets/pic3.png";
