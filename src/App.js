48bae8799600ebc04c966b28d3b8c9d85232952e
saraphina1 <oyeyemibunmi21@gmail.com>
2023-03-14 15:06:17 +0100

    made changes on the service page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
-
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import * as Pages from "./pages";
- import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
  
 
 
@@ -11,21 +10,15 @@ import * as Pages from "./pages";
 
 
 function App (){
-//   fetch("http://startupplanapiservice.onrender.com")
-//   .then((response)=> response.json())
-//   .then((json) => console.log(json))
-  
-
-
-
-
   return (
     <main className="w-full">
       <Routes>
         <Route path="/startup-plan" element={<Pages.Home />} />
         <Route path="/startup-plan/login" element={<Pages.Login />} />
         <Route path="/startup-plan/register" element={<Pages.Register />} />
-        <Route path="/startup-plan/OurServices" element={<Pages.OurServices />} />
+        <Route path="/OurServices" element={<Pages.OurServices />} />
+        <Route path="/contact-us" element={<Pages.ContactUs />} />
+        
       </Routes>
     </main>
     
---
+++ src/pages/ContactUs.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+// import ServicesNav from "../components/ServicesNav/ServicesNav";
+
+
+const ContactUs = () => {
+    return (
+      <div>
+      <section className="w-full">
+        <header className="w-full">
+            
+            {/* <ServicesNav /> */}
+
+
+
+
+            </header>
+            </section>
+            </div>
+    );
+};
+
+export default ContactUs;
